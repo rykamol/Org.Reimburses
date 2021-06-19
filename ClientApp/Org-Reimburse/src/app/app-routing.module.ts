8c44ffd06d34d614e8799baa8b54e28143f6e334
kamol roy <kamolroy.din@gmail.com>
2021-06-19 22:55:42 +0600

    ui rearange

--- ClientApp/Org-Reimburse/src/app/app-routing.module.ts
+++ ClientApp/Org-Reimburse/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { PaySalaryFormComponent } from './components/pay-salary-form/pay-salary-form.component';
+import { ClaimCourseFormComponent } from './components/claim-course-form/claim-course-form.component';
+import { ClaimCourseListComponent } from './components/claim-course-list/claim-course-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:ClaimCourseListComponent},
+  {path:"new-claim-course",component:ClaimCourseFormComponent},
+  {path:"pay-salary",component:PaySalaryFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ClientApp/Org-Reimburse/src/app/app.module.ts
+++ ClientApp/Org-Reimburse/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ClaimCourseFormComponent } from './components/claim-course-form/claim-course-form.component';
 import { ClaimCourseListComponent } from './components/claim-course-list/claim-course-list.component';
 import { PaySalaryFormComponent } from './components/pay-salary-form/pay-salary-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClaimCourseFormComponent,
     ClaimCourseListComponent,
-    PaySalaryFormComponent
+    PaySalaryFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
